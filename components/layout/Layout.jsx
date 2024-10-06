9a001b72fde3494a3fcbb4729c8bf998e8984a5c
juanpuertas <juanpuertas@gmail.com>
2024-10-06 16:32:40 -0400

    Hii new footer and function

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Backdrop, Box, Grid, Grid2, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography } from '@mui/material'
+import { Backdrop, Box, Divider, Grid, Grid2, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography } from '@mui/material'
 import { GitHub, TextSnippetRounded, WhatsApp } from '@mui/icons-material';
 import Navbar from '../ui/Navbar'
 import Head from 'next/head'
@@ -28,6 +28,13 @@ const Layout = ({children, title, pageDescription}) => {
                     <Grid2 container justifyContent="center">
                         <Grid2 size={{ xs: 12, md: 10, lg: 10, xl: 10 }}>
                             {children}
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 container justifyContent="center" mt={10}>
+                        <Grid2 size={{ xs: 12, md: 12, lg: 12, xl: 12 }}>
+                            <Divider sx={{ borderColor: '#fff', borderWidth: 1, borderStyle: 'solid' }} />
+                        </Grid2>
+                        <Grid2 size={{ xs: 12, md: 10, lg: 10, xl: 10 }}>
                             <Footer/>
                         </Grid2>
                     </Grid2>
@@ -41,18 +48,21 @@ const Layout = ({children, title, pageDescription}) => {
                     >
                         <SpeedDialAction
                             key="action1"
-                            icon={<TextSnippetRounded sx={{color:'#2e7ad1'}}/>}
+                            icon={<TextSnippetRounded sx={{color:'#2e7ad1'}} />}
                             tooltipTitle="Curriculum vitae"
+                            onClick={() => window.open('/CVJuanPuertas.pdf', '_blank')} // Cambia '/path/to/your/cv.pdf' por la ruta correcta a tu PDF en la carpeta public
                         />
                         <SpeedDialAction
                             key="action2"
-                            icon={<GitHub sx={{color:'#000'}}/>}
+                            icon={<GitHub sx={{color:'#000'}} />}
                             tooltipTitle="GitHub Profile"
+                            onClick={() => window.open('https://github.com/juanpuertas', '_blank')} // Cambia 'tu-usuario' por tu nombre de usuario de GitHub
                         />
                         <SpeedDialAction
                             key="action3"
-                            icon={<WhatsApp sx={{color:'#49ec72'}}/>}
+                            icon={<WhatsApp sx={{color:'#49ec72'}} />}
                             tooltipTitle="WhatsApp Number"
+                            onClick={() => window.open('https://wa.me/+584145512273?text=Hola%20quiero%20más%20información', '_blank')} // Cambia '1234567890' por tu número de WhatsApp
                         />
                     </SpeedDial>
                 </Box>
--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,30 +1,65 @@
-import { Box, Button, Grid2, Typography } from '@mui/material'
+import { Email, LocalPhone } from '@mui/icons-material'
+import { Box, Button, Divider, Grid2, Typography } from '@mui/material'
 import React from 'react'
-
+const infoContact = [
+    {
+        icon: <LocalPhone/>,
+        value: '+58 414-5512273'
+    },
+    {
+        icon: <></>,
+        value: '+58 412-7924415'
+    },
+    {
+        icon: <></>,
+        value: '+58 251-2734484'
+    },
+]
 const Footer = () => {
     return (
         <Grid2 container justifyContent='center' sx={{mt:10}}>
-            <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
-                <Typography variant='body1' fontWeight='bold' color='primary'>JuamPi</Typography>
+            
+            <Grid2 mt={1} size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+                <Typography variant='h1' fontWeight='medium' color='primary'>JuamPi</Typography>
             </Grid2>
-            <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+            <Grid2 mt={1} size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
                 <Box display='flex' flexDirection='column' gap={1} justifyContent='center'>
-                    <Typography variant='body1' color='primary'>+58 414-5512273</Typography>
-                    <Typography variant='body1' color='primary'>+58 412-7924415</Typography>
-                    <Typography variant='body1' color='primary'>+58 251-2734484</Typography>
-                    <Typography variant='body1' color='primary'><a href="">juanpuertas486@gmail.com</a></Typography>
+                    <Box display='flex' alignItems='center'>
+                        <LocalPhone sx={{color:'#fff'}}/>
+                        <Typography variant='body2' color='#fff'>+58 414-5512273</Typography>
+                    </Box>
+                    <Typography variant='body2' color='#fff' sx={{ml:{xs:3}}}>+58 412-7924415</Typography>
+                    <Typography variant='body2' color='#fff' sx={{ml:{xs:3}}}>+58 251-2734484</Typography>
+                    <Typography
+                        href="mailto:juanpuertas486@gmail.com" 
+                        component='a' 
+                        variant='body2' 
+                        color='#fff'
+                        display='flex' 
+                        alignItems='center'
+                        sx={{ 
+                            border: 'none', 
+                            textDecoration: 'none', 
+                            '&:hover': { color: 'primary.main' }, // Cambia 'primary.main' al color primario deseado
+                            '&:hover .MuiSvgIcon-root': { color: 'primary.main' } // Cambia el color del icono al pasar el mouse
+                        }} 
+                    >
+                        <Email sx={{ color: 'inherit' }} /> {/* Cambia el color del icono al color del Typography */}
+                        juanpuertas486@gmail.com
+                    </Typography>
                 </Box>
             </Grid2>
-            <Grid2 size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
+            <Grid2 mt={1} size={{ xs: 12, md: 6, lg: 4, xl: 3 }}>
                 <Box display='flex' justifyContent='end'>
                     <Button variant='contained' color='primary' sx={{borderRadius: 1}}>
                         <Typography variant='h6'>Contact me</Typography>
                     </Button>
                 </Box>
-                
             </Grid2>
-            <Grid2 size={{ xs: 12, md: 12, lg: 12, xl: 12 }}>
-                <Typography variant='h6' color='primary'>Footer</Typography>
+            <Grid2 mt={1} size={{ xs: 12, md: 12, lg: 12, xl: 12 }}>
+                <Typography variant='body2' color='#fff' textAlign='center' sx={{ mt: 2 }}>
+                    © {new Date().getFullYear()} Juampi. Todos los derechos reservados.
+                </Typography>
             </Grid2>
         </Grid2>
     )
